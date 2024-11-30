078a479ec8db4adc48d9a205feb2e36b335153b6
Felixmau360 <mfcolombo@gmail.com>
2024-11-30 20:14:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'QRcode',
+  webDir: 'www'
+};
+
+export default config;
