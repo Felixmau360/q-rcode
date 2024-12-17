a60b2b49db63514cdd65da1018e69c2544e4f370
Felixmau360 <mfcolombo@gmail.com>
2024-12-17 13:48:56 -0300

    v.000

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner'
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerScanResult } from '@capacitor/barcode-scanner'
 
 @Component({
   selector: 'app-home',
@@ -13,15 +13,23 @@ export class HomePage {
   resultado: string = "";
 
   async scanBarcode(val?: number) {
-    try{
-       const result = await CapacitorBarcodeScanner.scanBarcode({
+    try {
+      const result = await CapacitorBarcodeScanner.scanBarcode({
         hint: val || 17,
-        cameraDirection: 2
-        });
-        console.log(result);
-        return result.ScanResult;
-    } catch(e) {
-      throw(e);
+        cameraDirection: 1
+      });
+      
+      console.log(result);
+      this.resultado = result.ScanResult;
+    } catch(e: any) {
+      console.error('Erro ao escanear:', e);
+      if (e instanceof Error) {
+        if (e.message.includes('permission')) {
+          alert('É necessário permitir o acesso à câmera');
+        } else if (e.message.includes('OverconstrainedError')) {
+          alert('Erro ao acessar a câmera. Verifique se outro app não está usando a câmera.');
+        }
+      }
     } 
   }
 }
