c251735edc3c1e174a68a01f080bdcc4e081e463
Felixmau360 <mfcolombo@gmail.com>
2024-12-17 13:44:31 -0300

    v0000

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner'
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,18 @@ export class HomePage {
 
   constructor() {}
 
+  resultado: string = "";
+
+  async scanBarcode(val?: number) {
+    try{
+       const result = await CapacitorBarcodeScanner.scanBarcode({
+        hint: val || 17,
+        cameraDirection: 2
+        });
+        console.log(result);
+        return result.ScanResult;
+    } catch(e) {
+      throw(e);
+    } 
+  }
 }
